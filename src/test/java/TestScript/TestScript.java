73786eff054081c0832576d289ac802fca0052bd
Administrator <Administrator@172.31.8.69>
2023-10-03 07:04:35 +0000

    Initial Project

---
+++ src/test/java/TestScript/TestScript.java
@@ -0,0 +1,15 @@
+package TestScript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestScript {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+	}
+
+}
